3e15dbf07676b4e2e53986a9ccd963d8246b37f2
ramanakorada2611 <koradaramana2611@gmail.com>
2022-12-24 21:43:09 +0530

    first commit

---
+++ src/Data.jsx
@@ -0,0 +1,42 @@
+// import{Link} from 'react-router-dom'
+let data=[
+    {
+      id:0,
+      name:'ramana',
+      age:22,
+      course:'fsd',
+      batch:2022,
+      
+      // change:<Link to='/Editpage'>Edit</Link>
+    },
+    { id:1,
+      name:'ramana',
+      age:22,
+      course:'fsd',
+      batch:2022,
+      // change:<Link to='/Editpage'>Edit</Link>
+    },
+    {
+      id:2,
+      name:'ramana',
+      age:22,
+      course:'fsd',
+      batch:2022,
+      // change:<Link to='/Editpage'>Edit</Link>
+    },
+    {   id:3,
+        name:'ramana',
+        age:22,
+        course:'fsd',
+        batch:2022,
+        // change:<Link to='/Editpage'>Edit</Link>
+      },
+      { id:4,
+        name:'ramana',
+        age:22,
+        course:'fsd',
+        batch:2022,
+        // change:<Link to='/Editpage'>Edit</Link>
+      }
+  ]
+  export default data
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Home = () => {
+  return (
+    <>
+      <h2 className='text-center home'>Welcome to EveryOne🏠</h2>
+    </>
+
+  )
+}
